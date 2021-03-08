9fe8dd9dc5ce72146d5d6d17ed6d1adea7b077b4
LungWizard <46579370+LungWizard@users.noreply.github.com>
2021-03-07 20:36:48 -0500

    Add files via upload

--- UserInput.py
+++ UserInput.py
@@ -1,4 +1,35 @@
+import openpyxl
+from ProcessData import Create_OMETIFF
 #USER INPUT
 class UserInput():
-    GCI = 'TEST.gci'
-    img = '2.tif'
+    MainDirectory = 'C:'
+    SlideID = []
+    GroupID = []
+    XYID = []
+    GCI_List = []
+    ImgPath_List = []
+  #  GCI = []
+  #  ImgPath = []
+    WSI_List = openpyxl.load_workbook("OME-TIFF.xlsx").worksheets[0]
+    #WSI_Worksheet = SlideList.worksheets[0]
+    Num_Slides = WSI_List.max_row
+
+    X = 1
+
+    while X < Num_Slides:
+        X += 1
+        SlideID_Val = WSI_List.cell(row = X, column = 1).value
+        SlideID.append(SlideID_Val)
+        GroupID_Val = WSI_List.cell(row = X, column = 2).value
+        GroupID.append(GroupID_Val)
+        XYID_Val = WSI_List.cell(row = X, column = 3).value
+        XYID.append(XYID_Val)
+        
+    for X in range (0, Num_Slides):
+        GCI_List.append(MainDirectory + '/' + str(GroupID[X-1]) + '/' +
+                        XYID[X-1] + '/' + '*.gci')
+        ImgPath_List.append(MainDirectory + '/' + str(GroupID[X-1]) + '/' +
+                            XYID[X-1] + '/' + str(SlideID[X-1]) + '.tif')
+    
+    for X in range (0, Num_Slides):
+        Create_OMETIFF(GCI_List[X-1], ImgPath_List[X-1])
