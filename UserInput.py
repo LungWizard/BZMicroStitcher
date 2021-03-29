8edcc505b20083543eec7315fbc282c954548de0
LungWizard <46579370+LungWizard@users.noreply.github.com>
2021-03-28 23:49:18 -0400

    Added some lines to use the stitching function
    The stitching function was added to the ProcessFiles.py script

--- UserInput.py
+++ UserInput.py
@@ -1,46 +1,55 @@
 import openpyxl
-from ProcessData import Create_OMETIFF
-import pyprog
+from ProcessData import Create_OMETIFF, Stitch
+
 #USER INPUT
 class UserInput():
-    MainDirectory = 'D:/WSI/03012021'
+    ImgDate = '03182021'
+    OME_XLSX = 'OME-TIFF.xlsx'
+    MainDirectory = 'C:/' + ImgDate + '/'
+    WSI_List = openpyxl.load_workbook(MainDirectory+OME_XLSX).worksheets[0]
     SlideID = []
     GroupID = []
     XYID = []
     GCI_List = []
     ImgPath_List = []
-    WSI_List = openpyxl.load_workbook("D:/WSI/03012021/03012021.xlsx").worksheets[0]
+    Path_List = []
     Num_Slides = WSI_List.max_row
+    ErrorLog = open(MainDirectory + 'Error_Log_' + ImgDate + '.txt', 'a+')
 
-    X = 1
-
-    while X < Num_Slides:
-        X += 1
-        SlideID_Val = WSI_List.cell(row = X, column = 1).value
+    for X in range (1, Num_Slides):
+        SlideID_Val = WSI_List.cell(row = X+1, column = 1).value
         SlideID.append(SlideID_Val)
-        GroupID_Val = WSI_List.cell(row = X, column = 2).value
+        GroupID_Val = WSI_List.cell(row = X+1, column = 2).value
         GroupID.append(GroupID_Val)
-        XYID_Val = WSI_List.cell(row = X, column = 3).value
+        XYID_Val = WSI_List.cell(row = X+1, column = 3).value
         XYID.append(XYID_Val)
         
-    for X in range (0, Num_Slides):
-        GCI_List.append(MainDirectory + '/' + str(GroupID[X-1]) + '/' +
-                        str(XYID[X-1]) + '/' + '*.gci')
-        ImgPath_List.append(MainDirectory + '/' + str(GroupID[X-1]) + '/' +
-                            str(XYID[X-1]) + '/' + str(SlideID[X-1]) + '.tif') 
+    for Y in range (0, Num_Slides-1):
+        GCI_List.append(MainDirectory + '/' + str(GroupID[Y]) + '/' +
+                        str(XYID[Y]) + '/' + '*.gci')
+        
+        ImgPath_List.append(MainDirectory + '/' + str(GroupID[Y]) + '/' +
+                            str(XYID[Y]) + '/' + str(XYID[Y]) + '.tif') 
+        
+        Path_List.append(MainDirectory + '/' + str(GroupID[Y]) + '/' + 
+                            str(XYID[Y]))
 
-    ProgBar = pyprog.ProgressBar(" ", " ", total = Num_Slides, 
-                              bar_length = 26, 
-                              complete_symbol = "=", 
-                              not_complete_symbol = " ", 
-                              wrap_bar_prefix = " [", wrap_bar_suffix="] ",
-                              progress_explain = "", 
-                              progress_loc = pyprog.ProgressBar.PROGRESS_LOC_END)
-    
-    for X in range (0, Num_Slides):
+
+    for X in range (0, Num_Slides-1):
         try:
-            Create_OMETIFF(GCI_List[X-1], ImgPath_List[X-1])
-            ProgBar.set_stat(X + 1)
-            ProgBar.update()
+            Stitch(GCI_List[X], Path_List[X])
         except:
+            ErrorLog.write("Something is not right, unable to stitch: " + GCI_List[X] + ", " + 
+                  ImgPath_List[X] + ", " + SlideID [X] + "\n")
             pass
+    
+    for X in range (0, Num_Slides-1):
+        try:
+            Create_OMETIFF(MainDirectory, GCI_List[X], ImgPath_List[X], SlideID[X])
+        except:
+            ErrorLog.write("Something is not right, unable to create OME-TIFF: " 
+                           + GCI_List[X] + ", " + ImgPath_List[X] + ", " + SlideID [X] + "\n")
+            pass
+
+    ErrorLog.write("If this is the only line you see then everything went well! YAY!")
+    ErrorLog.close()
